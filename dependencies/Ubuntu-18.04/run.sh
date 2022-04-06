9720df5ed46a13b83e0e92db42c396074f9e48d7
Andrius Merkys <andrius.merkys@gmail.com>
2022-04-06 16:35:18 +0300

    Adding more dependencies.

--- dependencies/Ubuntu-18.04/run.sh
+++ dependencies/Ubuntu-18.04/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install --yes libhash-flatten-perl
-sudo apt install ncbi-blast+
-
+sudo apt-get install --yes libxml-simple-perl
+sudo apt-get install --yes ncbi-blast+
+sudo apt-get install --yes ncbi-entrez-direct
