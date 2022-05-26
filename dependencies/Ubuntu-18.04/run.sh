cfd9b2937058ddcfed037e9a23a293b785a6eafa
TDominyka <d.razanovaite@gmail.com>
2022-05-26 17:26:29 +0300

    R dependencies problem

--- dependencies/Ubuntu-18.04/run.sh
+++ dependencies/Ubuntu-18.04/run.sh
@@ -4,3 +4,14 @@ sudo apt-get install --yes libhash-flatten-perl
 sudo apt-get install --yes libxml-simple-perl
 sudo apt-get install --yes ncbi-blast+
 sudo apt-get install --yes ncbi-entrez-direct
+r-cran-biocmanager
+r-cran-matrixstats
+r-cran-cluster
+r-cran-latticeextra
+r-cran-hmisc
+r-cran-doparallel
+r-cran-fastcluster
+r-bioc-preprocesscore
+r-bioc-go.db r-bioc-impute
+r-bioc-annotationdbi
+libudunits2-dev
